0743fd8f3615e3bb9467f2e6662387029d801b76
LaxWasHere <LaxWasHere@PoweredByHate.net>
2014-07-12 02:08:24 -0700

    Done biaaatches!

--- src/main/java/com/tenjava/entries/LaxWasHere/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/LaxWasHere/t2/TenJava.java
@@ -1,5 +1,23 @@
 package com.tenjava.entries.LaxWasHere.t2;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin implements Listener {
+
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent ev) {
+        if(ev.getMessage().equalsIgnoreCase("imma chargin mah lazer")) {
+            Bukkit.broadcastMessage(ChatColor.RED + "" + ev.getPlayer().getName() + " IS HARNESSING THE ENERGY IN THE ATMOSPHERE TO CHARGE HIS LAZER!!!!!! OMG WTF");
+        }
+
+    }
+}
