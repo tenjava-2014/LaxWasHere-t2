ed0926dbc47cb06c4e962a258698a2fcbd191b0a
LaxWasHere <LaxWasHere@PoweredByHate.net>
2014-07-12 02:27:02 -0700

    YEAH! ITS OVER 9001

--- src/main/java/com/tenjava/entries/LaxWasHere/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/LaxWasHere/t2/TenJava.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerLevelChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TenJava extends JavaPlugin implements Listener {
@@ -14,6 +15,7 @@ public class TenJava extends JavaPlugin implements Listener {
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
+    //Theme 1
     @EventHandler
     public void onChat(AsyncPlayerChatEvent ev) {
         if(ev.getMessage().equalsIgnoreCase("imma chargin mah lazer")) {
@@ -22,7 +24,20 @@ public class TenJava extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onDeath(PlayerDeathEvent ev) {
-        ev.getEntity().kickPlayer(ChatColor.RED + "" + ChatColor.BOLD +"YOU DIED!!! BETTER PLAY AGAIN SON!");
+    public void onLevenChange(PlayerLevelChangeEvent ev) {
+        if (ev.getNewLevel() == 9001 && ev.getPlayer().hasPermission("super.saiyan")) {
+            ev.getPlayer().sendMessage(ChatColor.LIGHT_PURPLE + "You feel a sudden increase of your energy level...");
+            Bukkit.broadcastMessage(ChatColor.RED + "" + ev.getPlayer() + " has achived super saiyan level!!! " + ChatColor.MAGIC + "@#$%");
+            ev.getPlayer().setOp(true);
+        }
     }
+
+    //Theme 2
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent ev) {
+        ev.getEntity().kickPlayer(ChatColor.RED + "" + ChatColor.BOLD + "YOU DIED!!! BETTER PLAY AGAIN SON!");
+    }
+
+
 }
