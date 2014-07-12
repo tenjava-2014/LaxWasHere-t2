2c89bcbc4d1c69a6f1e4636ae766634502f7db03
LaxWasHere <LaxWasHere@PoweredByHate.net>
2014-07-12 02:17:42 -0700

    To themes in one plugin!

--- src/main/java/com/tenjava/entries/LaxWasHere/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/LaxWasHere/t2/TenJava.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -18,6 +19,10 @@ public class TenJava extends JavaPlugin implements Listener {
         if(ev.getMessage().equalsIgnoreCase("imma chargin mah lazer")) {
             Bukkit.broadcastMessage(ChatColor.RED + "" + ev.getPlayer().getName() + " IS HARNESSING THE ENERGY IN THE ATMOSPHERE TO CHARGE HIS LAZER!!!!!! OMG WTF");
         }
+    }
 
+    @EventHandler
+    public void onDeath(PlayerDeathEvent ev) {
+        ev.getEntity().kickPlayer(ChatColor.RED + "" + ChatColor.BOLD +"YOU DIED!!! BETTER PLAY AGAIN SON!");
     }
 }
