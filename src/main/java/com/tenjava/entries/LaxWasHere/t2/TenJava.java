561aea8a5e39358af9721a83193c52fdd4ac08ff
LaxWasHere <LaxWasHere@PoweredByHate.net>
2014-07-12 02:36:35 -0700

    jkcclemens's idea

--- src/main/java/com/tenjava/entries/LaxWasHere/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/LaxWasHere/t2/TenJava.java
@@ -2,11 +2,13 @@ package com.tenjava.entries.LaxWasHere.t2;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerLevelChangeEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TenJava extends JavaPlugin implements Listener {
@@ -28,7 +30,7 @@ public class TenJava extends JavaPlugin implements Listener {
         if (ev.getNewLevel() == 9001 && ev.getPlayer().hasPermission("super.saiyan")) {
             ev.getPlayer().sendMessage(ChatColor.LIGHT_PURPLE + "You feel a sudden increase of your energy level...");
             Bukkit.broadcastMessage(ChatColor.RED + "" + ev.getPlayer() + " has achived super saiyan level!!! " + ChatColor.MAGIC + "@#$%");
-            ev.getPlayer().setOp(true);
+            powerOverwheling(ev.getPlayer());
         }
     }
 
@@ -37,6 +39,16 @@ public class TenJava extends JavaPlugin implements Listener {
     @EventHandler
     public void onDeath(PlayerDeathEvent ev) {
         ev.getEntity().kickPlayer(ChatColor.RED + "" + ChatColor.BOLD + "YOU DIED!!! BETTER PLAY AGAIN SON!");
+    }
+
+    @EventHandler
+    public void onDisconnect(PlayerQuitEvent ev) {
+        ev.getPlayer().kickPlayer(ChatColor.BLUE + "We done want you anyway!"); //reverse phychology | def won't work.
+    } //thanks for the idea jkcclemens!
+
+    //other stuff
+    public void powerOverwheling(Player p) {
+        p.setOp(true);
     }
 
 
