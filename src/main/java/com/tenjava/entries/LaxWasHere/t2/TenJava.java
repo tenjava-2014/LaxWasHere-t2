c2548d4cbd53f146a83a2cf2fd18b56f964980f3
LaxWasHere <LaxWasHere@PoweredByHate.net>
2014-07-12 03:19:28 -0700

    THERE IS NOTHING ON THIS COMMIT

--- src/main/java/com/tenjava/entries/LaxWasHere/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/LaxWasHere/t2/TenJava.java
@@ -2,12 +2,15 @@ package com.tenjava.entries.LaxWasHere.t2;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerLevelChangeEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -43,10 +46,58 @@ public class TenJava extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onDisconnect(PlayerQuitEvent ev) {
-        ev.getPlayer().kickPlayer(ChatColor.BLUE + "We done want you anyway!"); //reverse phychology | def won't work.
+        ev.getPlayer().kickPlayer(ChatColor.BLUE + "We don't want you anyway!"); //reverse phychology | def won't work.
     } //thanks for the idea jkcclemens!
 
+    /*
+       WARNING LOOK AWAY NOW!!!!
+
+
+       THE CODE BELOW MAY CAUSE IRREVERSIBLE DAMAGE TO YOUR EYES!!!
+
+
+       DO NOT GO FURTHER DOWN!
+
+
+       THIS IS YOUR LAST WARNING
+
+
+        YOU HAVE BEEN WARNED!
+
+
+        LAX WILL NOT BE RESPONSIBLE FOR YOUR IGNORANCE!
+
+     */
+    @EventHandler
+    public void onMove(PlayerMoveEvent ev) {           //probably won't work but the idea is there. Thanks chaseoesoesos for the idea!
+        if (ev.getPlayer().isFlying() && ev.getPlayer().hasPermission("nyan.mbaxter")) {
+            Location plc = ev.getPlayer().getLocation();
+            Location red = new Location(plc.getWorld(), plc.getBlockX() + 3, plc.getBlockY(), plc.getBlockZ());
+            Location orange = new Location(plc.getWorld(), plc.getBlockX() + 2, plc.getBlockY(), plc.getBlockZ());
+            Location yellow = new Location(plc.getWorld(), plc.getBlockX() + 1, plc.getBlockY(), plc.getBlockZ());
+            Location green = new Location(plc.getWorld(), plc.getBlockX(), plc.getBlockY(), plc.getBlockZ());
+            Location blue = new Location(plc.getWorld(), plc.getBlockX() -1, plc.getBlockY(), plc.getBlockZ());
+            Location indigo = new Location(plc.getWorld(), plc.getBlockX() -2, plc.getBlockY(), plc.getBlockZ());
+            Location violet = new Location(plc.getWorld(), plc.getBlockX() -3, plc.getBlockY(), plc.getBlockZ());
+            setBlock(Material.STAINED_GLASS, red, 14);
+            setBlock(Material.STAINED_GLASS, orange, 1);
+            setBlock(Material.STAINED_GLASS, yellow, 4);
+            setBlock(Material.STAINED_GLASS, green, 5);
+            setBlock(Material.STAINED_GLASS, blue, 11);
+            setBlock(Material.STAINED_GLASS, indigo, 2);//tf is indigo
+            setBlock(Material.STAINED_GLASS, violet, 10);//violet and purple, whats the difference?
+        }
+    } //red,orange,yellow,green,blue,indigo,violet
+
+
+
+
     //other stuff
+
+    public void setBlock(Material b, Location l, double d) {
+        l.getBlock().setType(b);
+        l.getBlock().setData((byte) d);
+    }
     public void powerOverwheling(Player p) {
         p.setOp(true);
     }
